789b32822973adca842a64c2f040664318eead68
Anwesh43 <anweshthecool0@gmail.com>
2021-08-23 07:52:58 +0530

    adding ALTUNode class

--- index.ts
+++ index.ts
@@ -134,3 +134,45 @@ class Animator {
         }
     }
 }
+
+class ALTUNode {
+
+    next : ALTUNode 
+    prev : ALTUNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new ALTUNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawALTUNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+    
+    getNext(dir : number, cb : Function) : ALTUNode {
+        var curr : ALTUNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
