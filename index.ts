f7ebb2dc7bd7b685acb0fa67dac210e2e7608748
Anwesh43 <anweshthecool0@gmail.com>
2021-08-23 08:03:56 +0530

    adding ArrowLineToUp class

--- index.ts
+++ index.ts
@@ -176,3 +176,26 @@ class ALTUNode {
         return this 
     }
 }
+
+class ArrowLineToUp {
+
+    curr : ALTUNode = new ALTUNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
