0652df1e1623356ca7a3b8ab80909dc2de6e7d71
Anwesh43 <anweshthecool0@gmail.com>
2021-08-23 08:08:41 +0530

    adding drawing fix

--- index.ts
+++ index.ts
@@ -39,7 +39,7 @@ class DrawingUtil {
         const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
         const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
         context.save()
-        context.translate(w / 2, h / 2)
+        context.translate(w / 2, h / 2 - (h / 2 + size) * sc3)
         for (var j = 0; j < 2; j++) {
             context.save()
             context.scale(1 - 2 * j, 1)
